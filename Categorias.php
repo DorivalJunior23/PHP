e0068d8330d51d523f0de53ec7d40e6fa08151b1
Dorival Junior <89801891+DorivalJunior23@users.noreply.github.com>
2022-03-24 15:54:55 -0300

    Update Categorias.php

--- Categorias.php
+++ Categorias.php
@@ -1,13 +1,38 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>=Formulário de Inscrição</title>
+  <meta name="author" content="">
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+  
+<p>FORMULÁRIO PARA INSCRIÇÃO DE COMPETIDORES</p>
+
+<form action="script.php" method="post">
+  <p>Seu nome: <input type="text" name="nome" /></p>
+  <p>Sua idade: <input type="text" name="idade" /></p>
+  <p><input type="submit" /></p>
+</form>
+
+</body>
+
+</html>
+
+
 <?php
 $categorias = [];
 $categorias[]= 'Infantil';
 $categorias[]= 'Adolecente';
 $categorias[]= 'Livre';
 
-$nome = 'Dorival';
-$idade = 18;
+$nome = $_POST['nome'];
+$idade = $_POST['idade'];
 
-//var_dump($nome,$idade);
 
 if($idade<6){
 
